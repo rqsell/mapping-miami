a5833792f0e7d49e2bad48c2d7fbcd6b6b106393
Rebecca Sell <r.q.sell@gmail.com>
2026-01-27 21:22:57 -0500

    map entrance

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,18 +1,25 @@
 import   React from "react";
-
+import asset4 from "../images/asset-4.jpg";
+import bpc from "../images/bpc-logo.png";
+import kecc from "../images/kecc-white.png";
+import eight80 from "../images/880-white-horizontal.png";   
 import "../css/home.css";
 const Home = () => {
     return (
         <div className="home">
             <span className="welcome">welcome to</span>
-            <span className="mapping">MAPPING<br></br> MIAMI</span>
-            <span className="button">enter</span>
+            <span className="mapping">
+                <span>MAPPING</span>
+                <span style={{ fontSize: "1.4em" }}>MIAMI</span> 
+                </span>
+            <span className="button"><span className="letter">e</span><span className="letter">n</span><span className="letter">t</span><span className="letter">e</span> <span className="letter">r</span>
+
+          </span>
             <span className="project">A PROJECT BY MIRANDA DE GASPERI & BUEN PROVECHO COLLECTIVE<br></br>WITH SUPPORT FROM 8 80 CITIES & THE KNIGHT FOUNDATION</span>
             <span className="imageBox">
-                  <img src="../images/880-white-horizontal.png" alt="880" height="60" />
-        <img src="../images/asset-4.png" alt="Asset 4" height="60" />
-        <img src="../images/bpc-logo.png" alt="BPC" height="60" />
-        <img src="../images/kecc-white.png" alt="Knight" height="60" />
+                  <img src={eight80} alt="880" height="60" />
+        <img src={bpc} alt="BPC" height="60" />
+        <img src={kecc} alt="Knight" height="60" />
                 </span>
         </div>
     );
